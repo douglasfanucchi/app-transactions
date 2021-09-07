175830c64490208079e3bf0d2b68981972006ca7
Douglas Fanucchi <douglasfanucchi@gmail.com>
2021-09-07 10:53:08 -0300

    cria 10 usuarios clientes e 10 usuarios lojistas

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        User::factory()
+            ->count(10)
+            ->defineUserAsSeller()
+            ->create();
+
+        User::factory()
+            ->count(10)
+            ->create();
     }
 }
