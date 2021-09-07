ce3c17a6ab992b938a4af12bd577d3732f11572d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2021-09-07 09:16:36 -0300

    implementa model de usuario

---
+++ src/database/migrations/2021_09_07_114759_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['customer', 'seller']);
+            $table->string('name');
+            $table->string('document', 18)->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->float('credits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
